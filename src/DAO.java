48a67472dd4077be2c8ae803f066a7ff61f01aaa
Renos Kerkides <rkerkides@gmail.com>
2024-02-27 15:27:10 +0000

    add DAO interface and class

---
+++ src/DAO.java
@@ -0,0 +1,28 @@
+import java.util.Map;
+
+public class DAO implements TeacherDAO {
+    @Override
+    public Map<Integer, Teacher> getAllTeachers() {
+        return null;
+    }
+
+    @Override
+    public void addTeacher(Teacher teacher) {
+
+    }
+
+    @Override
+    public void removeTeacher(int id) {
+
+    }
+
+    @Override
+    public void updateTeacher(int id) {
+
+    }
+
+    @Override
+    public Teacher getTeacher(int id) {
+        return null;
+    }
+}
---
+++ src/TeacherDAO.java
@@ -0,0 +1,18 @@
+import java.util.Map;
+
+/*
+ * This interface is used to define the methods that will be used to interact with the database.
+ * It includes methods for CRUD (Create, Read, Update, Delete) operations.
+ * It can be implemented by both the current CSV implementation and a future database implementation.
+ * This allows the application to remain simple for now, whilst ensuring that it can be easily extended in the future.
+ */
+
+public interface TeacherDAO {
+    Map<Integer,Teacher> getAllTeachers();
+    void addTeacher(Teacher teacher);
+
+    void removeTeacher(int id);
+
+    void updateTeacher(int id);
+    Teacher getTeacher(int id);
+}
